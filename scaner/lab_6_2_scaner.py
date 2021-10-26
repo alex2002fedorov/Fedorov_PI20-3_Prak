6cbb701d805c17cf114e85209140ac066504c61c
Федоров Александр <90206728+alex2002fedorov@users.noreply.github.com>
2021-10-26 12:57:34 +0300

    Add files via upload

---
+++ scaner/lab_6_2_scaner.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+import pyprind
+
+N = 2 ** 16 - 1
+ip = input('Введите ip: ')
+
+
+def scan_port(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
+    try:
+        sock.connect((ip, port))
+        print('\n', "Порт", port, "открыт")
+        sock.close()
+    except:
+        pass
+
+
+bar = pyprind.ProgBar(N)
+for i in range(N):
+    potoc = threading.Thread(target=scan_port, args=(ip, i))
+    potoc.start()
+    bar.update()
